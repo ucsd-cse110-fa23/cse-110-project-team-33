b9191c5e9f3e6a63c4f54f83dcc5650cb72e6d9b
Igor Aprelev <igor.aprelev@gmail.com>
2023-11-03 08:46:40 -0700

    create class structure/hierarchy

--- PantryPal/src/App.java
+++ PantryPal/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        // initialize relevant classes
         System.out.println("Hello, World!");
     }
 }
---
+++ PantryPal/src/RecipeList.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecipeList {
+    private List<Recipe> recipes;
+    
+    public RecipeList() {
+        recipes = new ArrayList<Recipe>();
+    }
+
+    public void refresh() {
+        for (int i = 0; i < recipes.size(); i++) {
+            this.recipes.set(i, this.recipes.get(i));
+        }
+    }
+}
