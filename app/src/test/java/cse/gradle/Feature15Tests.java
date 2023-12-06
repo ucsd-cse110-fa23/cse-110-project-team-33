530787af0538d5933ea866c518eece12c5a933f7
notweezer123 <efentress@ucsd.edu>
2023-12-05 20:06:25 -0800

    Added Test to Feature 15

--- app/src/test/java/cse/gradle/Feature15Tests.java
+++ app/src/test/java/cse/gradle/Feature15Tests.java
@@ -1,5 +1,6 @@
 package cse.gradle;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 
@@ -71,6 +72,46 @@ public class Feature15Tests extends HTTPServerTests {
         // System.out.println(recipeArrayList.get(1).getName());
         assert(recipeArrayList.get(1).getName().equals("boiled potatoes"));
         assert(recipeArrayList.get(2).getName().equals("Healthy Lunch"));
+    }
+
+    @Test
+    public void getRecipeListZToATest() {
+
+        // Create a new List<Recipe> with 3 recipes
+        List<Recipe> recipes = new ArrayList<Recipe>();
+        
+        recipes.add(new Recipe("eggs, bacon", "cook for 10 minutes", "breakfast", "American breakfast"));
+        recipes.add(new Recipe("salmon, salad", "cook for 20 minutes", "lunch", "Healthy Lunch"));
+        recipes.add(new Recipe("potatoes", "boil the potatoes", "brunch", "boiled potatoes"));
+
+
+        // Create a new Mock Model that accesses test_user in the database
+        Model model = new MockModel();
+
+        // Add the recipes to the database
+        for (Recipe recipe : recipes) {
+            model.postRecipe(recipe);
+        }
+
+        // Get all the recipes from the database sorted alphabetically A-Z (not case sensitive)
+        String getAllResponse = model.getRecipeList("z-a", "");
+
+        // Delete the recipes from the database
+        for (Recipe recipe : recipes) {
+            model.deleteRecipe(recipe.getId().toString());
+        }
+
+        // Parse the getAllResponse into a List<Recipe>
+        List<Recipe> recipeArrayList = Recipe.parseRecipeListFromString(getAllResponse);
+
+        System.out.println("recipeArrayList: " + recipeArrayList);
+        
+        // Check that the recieved recipes are sorted alphabetically A-Z (not case sensitive)
+        assertEquals(3, recipeArrayList.size());
+        assert(recipeArrayList.get(2).getName().equals("American breakfast"));
+        // System.out.println(recipeArrayList.get(1).getName());
+        assert(recipeArrayList.get(1).getName().equals("boiled potatoes"));
+        assert(recipeArrayList.get(0).getName().equals("Healthy Lunch"));
 
     }
 }
