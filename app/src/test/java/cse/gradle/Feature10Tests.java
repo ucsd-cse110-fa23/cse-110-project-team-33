c64b27d673df6789a5552578d0733d3bfdba97b5
Igor Aprelev <igor.aprelev@gmail.com>
2023-12-05 23:07:48 -0800

    Feature10 tests done

--- app/src/test/java/cse/gradle/Feature10Tests.java
+++ app/src/test/java/cse/gradle/Feature10Tests.java
@@ -13,6 +13,36 @@ import java.util.ArrayList;
 
 class Feature10Tests {
     /* --------------------------------- UNIT TESTS --------------------------------- */
-    
+    @Test
+    void automaticLoginChecked() {
+        MockController controller = new MockController();
+        boolean loginStatus = false;
+        try {
+             loginStatus = controller.loginUser("abc", "abc");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        assert(!loginStatus);
+    }
     /* --------------------------------- BDD TESTS --------------------------------- */
+    @Test 
+    void loginLogoutWithAutoLogin() {
+        User Joe = new User("abc", "abc");
+        MockController controller = new MockController();
+        boolean loginStatus = false;
+        try {
+             loginStatus = controller.loginUser(Joe.getUsername(), Joe.getPassword());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        assert(!loginStatus);
+        Joe = new User("login", "checked");
+        loginStatus = true;
+        try {
+             loginStatus = controller.loginUser(Joe.getUsername(), Joe.getPassword());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        assert(loginStatus);
+    }
 }
